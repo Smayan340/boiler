c1ec8749de06aecc69bc162e7b12fbfc8a97ac61
Smayan340 <76809078+Smayan340@users.noreply.github.com>
2021-02-11 14:08:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,6 +11,11 @@ function preload()
 
 function setup() {
 	createCanvas(800, 700);
+	hammer1 = new Hammer(100,200,10,10);
+	plane1 = new Plane(300,200,10,10);
+	rubber1 = new Rubber(500,200,10,10);
+	stone1 = new Stone(700,200,10,10);
+		
 
 
 	engine = Engine.create();
@@ -27,6 +32,10 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+hammer1.display();
+	plane1.display();
+	rubber1.display();
+	stone1.display();
   
   drawSprites();
  
