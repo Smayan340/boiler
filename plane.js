0c804daef5c3158a2d2649845a46d5b7e4d2818c
Smayan340 <76809078+Smayan340@users.noreply.github.com>
2021-05-01 18:55:00 +0530

    Update plane.js

--- plane.js
+++ plane.js
@@ -1,9 +1,7 @@
 class Plane {
     constructor(x, y, width, height) {
       var options = {
-          'restitution':0.8,
-          'friction':1,
-          'density':1.0
+          isStatic: true
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
@@ -13,13 +11,9 @@ class Plane {
     }
     display(){
       var pos =this.body.position;
-      var angle = this.body.angle;
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
       rectMode(CENTER);
-      fill("grey");
-      rect(0, 0, this.width, this.height);
-      pop();
+      fill("brown");
+      rect(pos.x,pos.y, this.width, this.height);
+      
     }
   };
