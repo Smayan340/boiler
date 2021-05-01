c889fb94a7cb7d44ea056cb22f8879a0f0448522
Smayan340 <76809078+Smayan340@users.noreply.github.com>
2021-05-01 18:52:42 +0530

    Update hammer.js

--- hammer.js
+++ hammer.js
@@ -1,18 +1,20 @@
 class Hammer {
-    constructor(x, y, width, height) {
+    constructor(x, y) {
       var options = {
           'restitution':0.5,
           'friction':1,
           'density':2.0
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
+      this.width = 150;
+      this.height = 40;
       
       World.add(world, this.body);
-    }
+    };
     display(){
       var pos =this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
       var angle = this.body.angle;
       push();
       translate(pos.x, pos.y);
