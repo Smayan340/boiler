4ea0c4870aa314ffd9670722aa70b24963b30304
Smayan340 <76809078+Smayan340@users.noreply.github.com>
2021-05-01 18:54:09 +0530

    Update rubber.js

--- rubber.js
+++ rubber.js
@@ -1,25 +1,25 @@
 class Rubber {
-    constructor(x, y, width, height) {
+    constructor(x, y,r) {
       var options = {
           'restitution':0.8,
           'friction':5,
           'density':1.0
       }
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
       
+      this.r = r;
+      this.x = x;
+      this.y = y;
+      this.body= Bodies.circle(this.x,this.y,(this.r-20)/2,options)
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
-      var angle = this.body.angle;
       push();
       translate(pos.x, pos.y);
       rotate(angle);
       rectMode(CENTER);
-      fill("grey");
-      rect(0, 0, this.width, this.height);
+      fill("red");
+     elipse(0, 0, this.r);
       pop();
     }
   };
